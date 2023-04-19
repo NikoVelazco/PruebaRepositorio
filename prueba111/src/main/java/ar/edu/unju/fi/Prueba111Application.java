eef8c2de5eaaddec05180d8f6898e6608561a531
Cristian <Cristian@DESKTOP-5NQTU7T>
2023-04-19 17:14:04 -0300

    a cruzar los dedos

---
+++ prueba111/src/main/java/ar/edu/unju/fi/Prueba111Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prueba111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prueba111Application.class, args);
+	}
+
+}
